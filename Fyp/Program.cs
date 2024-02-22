9b60303a5ec3597ad905c56e07d6a3a5e282e0fe
prana <prana@DESKTOP-TT6T2IA>
2024-02-22 22:33:19 +0545

    Add project files.

---
+++ Fyp/Program.cs
@@ -0,0 +1,49 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Fyp.Data;
+using Fyp.Areas.Identity.Data;
+var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("FypContextConnection") ?? throw new InvalidOperationException("Connection string 'FypContextConnection' not found.");
+
+builder.Services.AddDbContext<FypContext>(options => options.UseSqlServer(connectionString));
+builder.Services.AddDefaultIdentity<Fypuser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<FypContext>();
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+app.MapRazorPages();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapGet("/", ctx =>
+{
+    if (ctx.User.Identity.IsAuthenticated)
+    {
+        // User is already authenticated, redirect to another page
+        ctx.Response.Redirect("/Home/Index");
+    }
+    else
+    {
+        // User is not authenticated, redirect to the login page
+        ctx.Response.Redirect("/Identity/Account/Login");
+    }
+
+    return Task.CompletedTask;
+});
+app.Run();
